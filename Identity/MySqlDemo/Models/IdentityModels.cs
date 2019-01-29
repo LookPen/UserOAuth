2b492adaf6289cb01a4320fff64d8a65ed089977
dzjx <502498669@qq.com>
2019-01-29 13:35:56 +0800

    Identity

---
+++ Identity/MySqlDemo/Models/IdentityModels.cs
@@ -0,0 +1,75 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MySqlDemo.Models
+{
+    // 可以通过向 ApplicationUser 类添加更多属性来为用户添加配置文件数据。若要了解详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=317594。
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // 请注意，authenticationType 必须与 CookieAuthenticationOptions.AuthenticationType 中定义的相应项匹配
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在此处添加自定义用户声明
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            if (modelBuilder == null)
+            {
+                throw new ArgumentNullException("modelBuilder");
+            }
+
+            var user = modelBuilder.Entity<ApplicationUser>()
+                .ToTable("AspNetUsers");
+            user.HasMany(u => u.Roles).WithRequired().HasForeignKey(ur => ur.UserId);
+            user.HasMany(u => u.Claims).WithRequired().HasForeignKey(uc => uc.UserId);
+            user.HasMany(u => u.Logins).WithRequired().HasForeignKey(ul => ul.UserId);
+            user.Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(255) // 修改长度 避免Mysql 索引超出范围
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("UserNameIndex") { IsUnique = true }));
+
+            user.Property(u => u.Email).HasMaxLength(256);
+
+            modelBuilder.Entity<IdentityUserRole>()
+                .HasKey(r => new { r.UserId, r.RoleId })
+                .ToTable("AspNetUserRoles");
+
+            modelBuilder.Entity<IdentityUserLogin>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey, l.UserId })
+                .ToTable("AspNetUserLogins");
+
+            modelBuilder.Entity<IdentityUserClaim>()
+                .ToTable("AspNetUserClaims");
+
+            var role = modelBuilder.Entity<IdentityRole>()
+                .ToTable("AspNetRoles");
+            role.Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(255)// 修改长度 避免Mysql 索引超出范围
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("RoleNameIndex") { IsUnique = true }));
+            role.HasMany(r => r.Users).WithRequired().HasForeignKey(ur => ur.RoleId);
+        }
+    }
+}
